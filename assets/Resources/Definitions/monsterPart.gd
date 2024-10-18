8d42c04a1e946406ee0e7c8214f0f415075f353c
Filip Bednář <f.bednar.st@spseiostrava.cz>
2024-10-18 14:21:52 +0200

    veci

---
+++ assets/Resources/Definitions/monsterPart.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name monsterPart
+
+@export var partName : String
+@export var texture : ImageTexture
+@export var icon : ImageTexture
+@export var stats : Dictionary
---
+++ scenes/MonsterMaker/monsterPart/monster_part.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var monsterPart : monsterPart
+var monsterPartStats : Dictionary
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$TextureRect.texture = monsterPart.icon
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
