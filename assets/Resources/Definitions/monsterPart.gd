f15e47edf533a91f21eb194440be5d73daa14840
Filip Bednář <f.bednar.st@spseiostrava.cz>
2024-10-18 16:58:16 +0200

    ResUpdate2

--- assets/Resources/Definitions/monsterPart.gd
+++ assets/Resources/Definitions/monsterPart.gd
@@ -3,6 +3,7 @@ extends Resource
 class_name monsterPart
 
 @export var partName : String = ""
+@export var description : String = ""
 @export var texture : ImageTexture
 @export var icon : ImageTexture
 @export var stats : Dictionary = {
